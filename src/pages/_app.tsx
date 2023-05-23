dc9b32d36ddd2fbfadac5eda5ac03d4873bc7894
Noud Verstijnen <noud.verstijnen@gmail.com>
2023-05-23 22:43:31 +0200

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import "@/styles/portfolio.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Navigation from "@/components/navigation";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <div className="content">
         <Component {...pageProps} />
       </div>
+      <Analytics />
     </>
   );
 }
